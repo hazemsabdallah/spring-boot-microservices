9bbaec90f78f4cf3434348b565ed0d7aca82efd7
Hazem Samir <hazemsabdallah@gmail.com>
2024-09-27 00:30:09 +0300

    using RestTemplate to communicate between microservices

--- movie-catalog-service/src/main/java/com/mircroservices/movie_catalog_service/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/mircroservices/movie_catalog_service/controller/MovieCatalogController.java
@@ -1,22 +1,41 @@
 package com.mircroservices.movie_catalog_service.controller;
 
 import com.mircroservices.movie_catalog_service.model.CatalogItem;
+import com.mircroservices.movie_catalog_service.model.Movie;
+import com.mircroservices.movie_catalog_service.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public MovieCatalogController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalogItems(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem("Movie 01", "Movie 01", 5)
+
+        List<Rating> ratings = List.of(
+                new Rating("01", 5),
+                new Rating("02", 4)
         );
+
+        return ratings.stream()
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "test desc", rating.getRating());
+                })
+                .toList();
     }
 }
