8d0efe33f5191f0d1f1a1886f7e7fb2ffb50d15c
Hazem Samir <hazemsabdallah@gmail.com>
2024-10-26 22:31:26 +0300

    storing config vars in list/map

--- spring-boot-config/src/main/java/com/mircroservices/spring_boot_config/GreetingController.java
+++ spring-boot-config/src/main/java/com/mircroservices/spring_boot_config/GreetingController.java
@@ -4,14 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class GreetingController {
 
-    @Value("${my.greeting}")
+    @Value("${my.greeting: default value}")
     private String greeting;
+
+    @Value("${my.greeting.list: default value}")
+    private List<String> greetingsList;
+
+    @Value("#{${my.map}}")
+    private Map<String, String> map;
 
     @GetMapping(value = "/greeting")
     public String greeting() {
-        return greeting;
+        return greeting + " " + greetingsList + " " + map;
     }
 }
